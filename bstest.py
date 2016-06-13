b916d0b1a06230812354f3cb3b2acf362a2c1a60
Nicholas Dry <nicholasdry@me.com>
2016-06-13 13:09:41 -0400

    Updated

--- bstest.py
+++ bstest.py
@@ -1,7 +1,6 @@
 # Nicholas Dry http://www.nicholasdry.com
 # Copyright 2016
 
-# TODO: Develop way to determine whether it will be Gradebook or Gradebook 2 which we will attempt to access.
 # TODO: Allow for the checking of more than one class
 
 from bs4 import BeautifulSoup   # This module allows for the scraping of the website.
@@ -10,6 +9,20 @@ import time # This module allows for the sleep function to be called.
 import os
 import sys
 
+pastTextFile = []
+current = []
+
+# TODO: Saving of a list into a text file, then loading it back into a dictionary
+# This method pulls a past text file called past.txt and saves it into a list,
+def loadPastTextFile():
+    with open('past.txt') as f:
+        pastTextFile.append(f.read().split())
+
+def checkSimilarity():
+    if current == pastTextFile:
+        print("same")
+    else:
+        print("not same")
 
 # This method is for anyone on Mac OSX who wishes to receive notifications of when the grades are posted through Notification Center.
 def sendNotification(title, subtitle, message):
@@ -17,7 +30,6 @@ def sendNotification(title, subtitle, message):
     s = '-subtitle {!r}'.format(subtitle)
     m = '-message {!r}'.format(message)
     os.system("terminal-notifier {}".format(' '.join([t, s, m])))
-
 
 # This method is for anyone who wishes to receive notifications of when the grades are posted through email.
 def sendEmail():
@@ -52,24 +64,46 @@ def gradebookTwo():
     assignmentNames = gradebook.find_all("div", {"class": "x-grid3-cell-inner x-grid3-col-S_ITM_NM"}) # This grabs the assignment names
     assignmentGrades = gradebook.find_all("div", {"class": "x-grid3-cell-inner x-grid3-col-S_GRD"}) # This grads the assignment grades
 
-    gradeDictionary = {}    # Empty dictionary for us to assign the values to.
+    for i in assignmentNames:
+        current.append(i)
 
-    for i, j in zip(assignmentNames, assignmentGrades): # This formats the dictionary nicely.
-        gradeDictionary[i.get_text()] = j.get_text()
+    for i in assignmentGrades:
+        current.append(i)
 
-    print(gradeDictionary)
+    past = open("current.txt", "w")
+    for i in current:
+        past.write("%s\n" % i)
 
-driver = webdriver.Firefox()  # PhantomJS allows the script to run without opening up Firefox or any browser
-driver.get("https://cas.rutgers.edu/login?service=https%3A%2F%2Fsakai.rutgers.edu%2Fsakai-login-tool%2Fcontainer")  # This is the sakai homepage
+    with open('current.txt') as f:
+        current.append(f.read().split())
 
-username_field = driver.find_element_by_name("username") # Sakai nicely names its fields.
-password_field = driver.find_element_by_name("password")
-username_field.send_keys("nsd48") # User NetID
-password_field.send_keys("Mustafa12578") # User
-password_field.submit() # submit it
 
-# This is the gradebook which are attempting to access
-driver.get("https://sakai.rutgers.edu/portal/site/848d9ee2-3e91-4935-bed6-ac8e4c77f22c/page/9f5d49e0-8574-4395-b192-65b0b52780ec")
+
+loadPastTextFile()
+
+attempts = 0
+
+while attempts < 3:
+    try:
+        driver = webdriver.Firefox()  # PhantomJS allows the script to run without opening up Firefox or any browser
+        driver.get("https://cas.rutgers.edu/login?service=https%3A%2F%2Fsakai.rutgers.edu%2Fsakai-login-tool%2Fcontainer")  # This is the sakai homepage
+
+        username_field = driver.find_element_by_name("username") # Sakai nicely names its fields.
+        password_field = driver.find_element_by_name("password")
+        username_field.send_keys("nsd48") # User NetID
+        password_field.send_keys("Mustafa12578") # User
+        password_field.submit() # submit it
+
+        # This is the gradebook which are attempting to access
+        driver.get("https://sakai.rutgers.edu/portal/site/7d6cf024-b944-4e38-a21a-06f73f427ce4/page/fedd6c39-4d6a-4a46-8a2f-f8aaf97b5df4")
+        break
+    except:
+        print("Connection Error: Trying Again")
+        attempts = attempts + 1
+
+if attempts == 3:
+    sendNotification("ERROR", "Unable to Connect to Sakai", "Internet Connection Not Available")
+    exit()
 
 frame = driver.find_element_by_tag_name("iframe")   # We want to grab the iframe.
 driver.switch_to_frame(frame)   # Now we send our driver to that new frame which allows us to access the page source below.
@@ -80,12 +114,10 @@ html = driver.page_source   # Grab the source code the the iframe widget.
 gradebook = BeautifulSoup(html, "html.parser")  # Begin the segmenting and parsing.
 
 # This determines what version gradebook Sakai is using.
-
-print("Made it")
-
 if gradebook.title.string == "Gradebook":
     gradebookTwo()
 else:
     gradebookOne()
 
 # TODO: Send notification, Title = Class Name, Subtitle = Assignment Name, Message = Assignment Grade
+checkSimilarity()
